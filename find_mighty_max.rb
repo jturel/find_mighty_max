3b4cf826347e9417f28555f3e44567cca3d9eb9d
Jonathon Turel <jturel@redhat.com>
2016-01-15 22:07:27 -0500

    Functionized

--- find_mighty_max.rb
+++ find_mighty_max.rb
@@ -4,12 +4,27 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-html = Nokogiri::HTML(open("http://www.lkqpickyourpart.com/DesktopModules/pyp_vehicleInventory/getVehicleInventory.aspx?store=142&page=0&filter=dodge&sp=&cl=&carbuyYardCode=1142&pageSize=15&language=en-US"))
+LKQ_RALEIGH = 1168
+LKQ_DURHAM = 1142
 
+def lookup_lkq(search_string, store_number)
+  puts "Results for '#{search_string} from #{store_number}"
+  html = Nokogiri::HTML(open("http://www.lkqpickyourpart.com/DesktopModules/pyp_vehicleInventory/getVehicleInventory.aspx?page=0&filter=#{search_string}&sp=&cl=&carbuyYardCode=#{store_number}&pageSize=15&language=en-US"))
+  html.xpath('//html/body/tr').each do |row|
 
-html.xpath('//html/body/tr').each do |row|
+    make = row.xpath('td[@class="pypvi_make"]/text()')
+    model = row.xpath('td[@class="pypvi_model"]/text()')
+    year = row.xpath('td[@class="pypvi_year"]/text()')
+    date = row.xpath('td[@class="pypvi_date"]/text()')
+    puts "#{make} #{model} #{year} #{date}"
 
-  make = row.xpath('td[@class="pypvi_make"]/text()')
-  puts make
-
+    #binding.pry
+  end
 end
+
+
+lookup_lkq('dodge', LKQ_RALEIGH)
+
+puts "\n"
+
+lookup_lkq('dodge', LKQ_DURHAM)
